658371a8ab4bfbdf0c6aed7130b58efec28df3ae
byteom <150465879+byteom@users.noreply.github.com>
2023-11-14 23:06:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('submit-btn').addEventListener('click', function () {
+    // Replace '' with your actual Gmail address
+    window.location.href = 'mailto:123juhi@gmail.com';
+});
